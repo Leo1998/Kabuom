76a3b18c3af8e594d11146c9d84473068029b2f3
Leonard <leonard.fricke98@gmail.com>
2016-06-08 17:34:49 +0200

    shaders

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -7,6 +7,20 @@ import org.lwjgl.opengl.GL11;
 
 public class ViewManager {
 
+    private static ShaderProgram createShader() {
+        String vertexSource = "void main() {" +
+                "gl_Position = gl_Vertex;" +
+                "}";
+
+        String fragmentSource = "void main() {" +
+                "gl_FragColor = vec4(0, 1, 0, 1);" +
+                "}";
+
+        return new ShaderProgram(vertexSource, fragmentSource);
+    }
+
+    private ShaderProgram shader;
+
     public ViewManager() {
         try {
             Display.setDisplayMode(new DisplayMode(800, 600));
@@ -14,28 +28,30 @@ public class ViewManager {
             Display.setTitle("Kabuom! Tower Defense");
 
             Display.create();
+
+            System.out.println("OpenGL context created! Version: " + GL11.glGetString(GL11.GL_VERSION));
         } catch (LWJGLException e) {
             e.printStackTrace();
             System.exit(0);
         }
+
+        shader = createShader();
     }
 
     public void render(float deltaTime) {
+        if (Display.wasResized()) {
+            GL11.glViewport(0, 0, Display.getWidth(), Display.getHeight());
+        }
+
         GL11.glClearColor(1f, 0f, 0f, 1f);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, Display.getWidth(), 0, Display.getHeight(), 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        shader.use();
 
-        GL11.glColor3f(0.5f,0.5f,1.0f);
-
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2f(100,100);
-        GL11.glVertex2f(100+200,100);
-        GL11.glVertex2f(100+200,100+200);
-        GL11.glVertex2f(100,100+200);
+        GL11.glBegin(GL11.GL_TRIANGLES);
+        GL11.glVertex2f(0.0f,0.0f);
+        GL11.glVertex2f(0.5f, 1.0f);
+        GL11.glVertex2f(1.0f,0.0f);
         GL11.glEnd();
 
         Display.update();
