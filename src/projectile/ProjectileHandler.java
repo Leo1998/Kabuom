c298a51a141f3c009cbb9124bd11db2840af2cbd
Hinke <HELLO@l.l>
2016-06-09 18:28:01 +0200

    Delete

--- src/projectile/ProjectileHandler.java
+++ src/projectile/ProjectileHandler.java
@@ -4,9 +4,7 @@ import enemy.Enemy;
 import graph.List;
 import utility.Vector2;
 
-/**
- * Created by Daniel on 09.06.2016.
- */
+
 public class ProjectileHandler {
     public void handleProjectiles(float dt,List<Projectile> projectiles,List<Enemy> enemies){
 
@@ -14,9 +12,4 @@ public class ProjectileHandler {
 
     }
 
-    public boolean isColliding(Projectile p, Enemy e){
-        if(new Vector2(p.getX(),p.getY(),e.getX(),e.getY()).getLength() <= ){
-
-        }
-    }
 }
