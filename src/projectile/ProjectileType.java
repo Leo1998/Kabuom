8ff9acaae8d2748cb81b14975d9bd9d46800c55f
Niclas <nicis-mail@t-online.de>
2016-06-17 13:08:09 +0200

    hoffe das passt so, habe Tower geaddet und ProjectileType Dummies

--- src/projectile/ProjectileType.java
+++ src/projectile/ProjectileType.java
@@ -1,6 +1,15 @@
 package projectile;
 
-
+/**
+ * Created by Daniel on 09.06.2016.
+ */
 public enum ProjectileType {
-
+    BULLET ,
+    MISSILE ,
+    FLAME ,
+    ICE ,
+    LIGHTNING ,
+    PIERCINGBULLET ,
+    FRAGGRANADE ,
+    POISON ;
 }
---
+++ src/tower/TowerType.java
@@ -0,0 +1,34 @@
+package tower;
+
+import projectile.ProjectileType;
+
+/**
+ * Created by Niclas on 15.06.2016.
+ */
+public enum TowerType{
+
+    MGTURRET(100, 0.1f,250,100,ProjectileType.BULLET),
+    MISSILELAUNCHER(75,2,400,200,ProjectileType.MISSILE),
+    POISONTOWER(75,3,200,175,ProjectileType.POISON),
+    CYROGUN(90,5,200,350,ProjectileType.ICE),
+    TESLACOIL(50,0.001f,150,500,ProjectileType.LIGHTNING),
+    FLAMETHROWER(60,0.001f,200,300,ProjectileType.FLAME),
+    SNIPER(20,4,2000,250,ProjectileType.PIERCINGBULLET),
+    MORTAR(40,6,1500,750,ProjectileType.FRAGGRANADE),
+    BARRICADE(250,0,0,50,null),
+    MAINTOWER(1000,0,0,0,null),
+    DUMMY(0,0,0,0,null);
+
+    int HP,attackRadius,cost;
+    float frequency;
+    ProjectileType projectileType;
+
+    TowerType(int Hp, float frequency, int attackRadius, int cost, ProjectileType proType){
+        HP = Hp;
+        projectileType=proType;
+        this.frequency = frequency;
+        this.attackRadius = attackRadius;
+        this.cost=cost;
+
+    }
+}
