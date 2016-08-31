749fccf861f609d93564efb4eff627034a5437e1
Daniel <hi@swag.ultimateRIPMasters.onion>
2016-08-31 17:33:46 +0200

    adwodfjgjndfbgfhn

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -71,7 +71,7 @@ public class GameView extends View{
     public void drawGameObject(GameObject o,Batch batch){
          if(o instanceof Enemy){
              Enemy e = (Enemy) o;
-
+             
              float percentage = (float) e.getMaxHp() / (float) e.getHp();
 
              batch.draw(ViewManager.getTexture(e.getEnemyType().getTextureID()), blockCoordToViewCoordX(e.getX()),blockCoordToViewCoordY(e.getY()),(h2/world.getBlocks().length),h2/world.getBlocks().length);
