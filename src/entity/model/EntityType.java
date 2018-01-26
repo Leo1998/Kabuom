179fb236c6248832dfdfa4470852bec3205c53d6
DanielAlexA <d.albert.1@web.de>
2018-01-26 17:52:49 +0100

    EntityType verbesserungen

--- src/entity/model/EntityType.java
+++ src/entity/model/EntityType.java
@@ -1,7 +1,6 @@
 package entity.model;
 
 import model.ObjectType;
-import projectile.Projectile;
 import projectile.ProjectileType;
 
 public enum EntityType implements ObjectType {
@@ -15,7 +14,7 @@ public enum EntityType implements ObjectType {
     SNIPER(     20,     0.325f, 0,      "Sniper",       "LaserLampRot.png", 3,          16,     0,      1,  false,  true,   false,  true,   ProjectileType.PIERCINGBULLET,  500),
     MORTAR(     40,     0.625f, 0,      "Mortar",       "MissileLauncherGruen.png",4,   20,     0.01f,  1,  false,  true,   false,  true,   ProjectileType.FRAGGRENADE,     750),
     BARRICADE(  1000,   0.625f, 0,      "Barricade",    "barricade2.png",   0,          1,      1,      8,  false,  true,   false,  true,   null,                           75),
-    MAINTOWER(  1000,   0.5f,   0,      "Maintower",    "MainTower.png",    0,          0,      0,      0,  false,  false,  false,  false,  null,                           0),
+    MAINTOWER(  1000,   0.625f, 0,      "Maintower",    "MainTower.png",    0,          0,      0,      0,  false,  false,  false,  false,  null,                           0),
     //MAINTOWER(  100000, 2,      0,      "OP Tower",     "MainTower.png",    0.03125f,   20,     7,      128,false,  true,   false,  true,   ProjectileType.LIGHTNING        0),
 
     //Enemy     maxHp   radius  speed   name            textureId           frequency   range   accur   att aAlly   aHost   hAlly   hHost   projectile                      cost
